73292057da7c269e6bd5ae014d1827f13bfa4028
Yukun Guo <gyk.net@gmail.com>
2014-10-22 17:22:56 +0800

    Add CONFIG.m

---
+++ CONFIG.m
@@ -0,0 +1,11 @@
+classdef CONFIG
+	properties (Constant)
+
+	% background means and variances
+	BG_PATH = 'F:\HumanEva\Background\';
+
+	% frames of videos will be saved here
+	SNAPSHOT_PATH = 'F:\silhouette\video\';
+
+	end
+end
